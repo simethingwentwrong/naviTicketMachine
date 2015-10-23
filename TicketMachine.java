4c3fc51db33f4dbed074e2b33860dc2c1f5cd0cb
Yggdrasil <ryuk1967@gmail.com>
2015-10-23 13:27:25 +0200

    añadida segunda maquina

--- TicketMachine.java
+++ TicketMachine.java
@@ -23,9 +23,19 @@ public class TicketMachine
      * Note that the price must be greater than zero, and there
      * are no checks to ensure this.
      */
-    public TicketMachine(int cost)
+    public TicketMachine()
     {
         price = 800;
+        balance = 0;
+        total = 0;
+    }
+    
+    /**
+     * Crea una maquina que te permite fijar el precio del billete
+     */
+    public TicketMachine (int cost)
+    {
+        price = cost;
         balance = 0;
         total = 0;
     }
